467b2c5e0f3df31a705a50bc75057e3a36e928c8
Jason Song <jsong0327@naver.com>
2017-12-27 15:55:40 +0900

    Small image loaded

--- app/src/main/java/com/example/user/project1/Fragment2.java
+++ app/src/main/java/com/example/user/project1/Fragment2.java
@@ -1,14 +1,21 @@
 package com.example.user.project1;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
@@ -17,6 +24,8 @@ import java.util.ArrayList;
  */
 public class Fragment2 extends Fragment {
 
+    private Button btnTEST;
+
     public Fragment2() {
         // Required empty public constructor
     }
@@ -24,9 +33,49 @@ public class Fragment2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_fragment2, container, false);
         // Inflate the layout for this fragment
-        //ArrayList<String> imageList = getAllShownImagesPath(getActivity());
-        return inflater.inflate(R.layout.fragment_fragment2, container, false);
+        Log.d("Fragment2", "Starting log");
+        Activity thisActivity = getActivity();
+        // Here, thisActivity is the current activity
+        int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 2;
+        if (ContextCompat.checkSelfPermission(thisActivity,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(thisActivity,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(thisActivity,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+        ArrayList<String> imageList = getAllShownImagesPath(getActivity());
+        for (int i = 0; i<imageList.size(); i++) {
+            Log.d("Fragment2", i+"'rd imageList: "+imageList.get(i));
+        }
+        Log.d("Fragment2", "There were a total of "+imageList.size()+"images on this phone");
+
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageView2);
+        Uri imgUri = Uri.parse(imageList.get(0));
+        imageView.setImageURI(null);
+        imageView.setImageURI(imgUri);
+
+        return view;
     }
 
     public static ArrayList<String> getAllShownImagesPath(Activity activity) {
