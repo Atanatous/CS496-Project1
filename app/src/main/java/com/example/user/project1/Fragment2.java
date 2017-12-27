353a947b04e262b04bcd3ba1cf795b0a95c8e757
Jason Song <jsong0327@naver.com>
2017-12-27 11:28:21 +0900

    First try of getAllShownImagesPath

--- app/src/main/java/com/example/user/project1/Fragment2.java
+++ app/src/main/java/com/example/user/project1/Fragment2.java
@@ -1,10 +1,16 @@
 package com.example.user.project1;
 
+import android.app.Activity;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,7 +25,29 @@ public class Fragment2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        //ArrayList<String> imageList = getAllShownImagesPath(getActivity());
         return inflater.inflate(R.layout.fragment_fragment2, container, false);
     }
+
+    public static ArrayList<String> getAllShownImagesPath(Activity activity) {
+        Uri uri;
+        Cursor cursor;
+        int column_index_data, column_index_folder_name;
+        ArrayList<String> listOfAllImages = new ArrayList<String>();
+        String absolutePathOfImage = null;
+        uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
+        String[] projection = {MediaStore.MediaColumns.DATA, MediaStore.Images.Media.BUCKET_DISPLAY_NAME};
+        cursor = activity.getContentResolver().query(uri, projection, null, null, null);
+        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        column_index_folder_name = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+        while(cursor.moveToNext()) {
+            absolutePathOfImage = cursor.getString(column_index_data);
+            listOfAllImages.add(absolutePathOfImage);
+        }
+        return listOfAllImages;
+
+    }
+
 
 }
